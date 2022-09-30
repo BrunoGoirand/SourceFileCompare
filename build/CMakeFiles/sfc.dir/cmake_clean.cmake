557a4c4cebaac6c331b0e369ae9a2d988df34de1
Bruno Goirand <geebe@goirand.com>
2022-09-30 18:51:19 +0200

    Ouf, at least, this is a working release

---
+++ build/CMakeFiles/sfc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfc.dir/sfc.c.o"
+  "CMakeFiles/sfc.dir/sfc.c.o.d"
+  "sfc"
+  "sfc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sfc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- sfc.c
+++ sfc.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "stringutil.h"
+#include <NeatString/NeatString.h>
 
 /* returns
 
@@ -17,6 +17,7 @@
 #define RMV_CMTS  5 // remove comments
 #define RMV__SPC  6 // remove spaces
 
+#define MAX_CHAR  160
 
 /*
 ** all functions return 0 when successful
@@ -27,16 +28,16 @@
 */
 int validateArguments(int argc, char **argv) {
 
-  fprintf(stderr, "# args: %d\n", argc);
-  for(int i=1; i<argc; i++)
-    fprintf(stderr, "arg[%d]: %s(%lu)\n", i, argv[i], strlen(argv[i]));
+	fprintf(stderr, "# args: %d\n", argc);
+	for(int i=1; i<argc; i++)
+		fprintf(stderr, "arg[%d]: %s(%lu)\n", i, argv[i], strlen(argv[i]));
 
-  if(argc != 4) {
-    fprintf(stdout, "Usage: sfc dir1 dir2 file\n");
-    return(1);
-  }
+	if(argc != 4) {
+		fprintf(stdout, "Usage: sfc dir1 dir2 file\n");
+		return(1);
+	}
 
-  return(0);
+	return(0);
 }
 
 /*
@@ -44,27 +45,27 @@ int validateArguments(int argc, char **argv) {
 */
 int buildFilename(int argc, char **argv, int arg, char **fn) {
 
-  int length;
-  //char *so; // string operation
-  // 
-  length = strlen(argv[arg])+strlen(argv[3])+2;
-  if(length > FILENAME_MAX) {
-    fprintf(stderr, "filename is too long\n");
-    return(1);
-  }
+	int length;
+	//char *so; // string operation
+	// 
+	length = strlen(argv[arg])+strlen(argv[3])+2;
+	if(length > FILENAME_MAX) {
+		fprintf(stderr, "filename is too long\n");
+		return(1);
+	}
 
-  *fn = (char *) malloc(length);
-  if(!*fn) {
-    fprintf(stderr, "unable to allocate filename length\n");
-    return(1);
-  }
+	*fn = (char *) malloc(length + 1);
+	if(!*fn) {
+		fprintf(stderr, "unable to allocate filename length\n");
+		return(1);
+	}
 
-  if(strcpy(*fn, argv[arg]) != *fn) return(1);
-  if(strcat(*fn, "/") != *fn) return(1);
-  if(strcat(*fn, argv[3]) != *fn) return(1);
+	if(strcpy(*fn, argv[arg]) != *fn) return(1);
+	if(strcat(*fn, "/") != *fn) return(1);
+	if(strcat(*fn, argv[3]) != *fn) return(1);
 
-  fprintf(stderr, "file name [%d]: %s\n", arg, *fn);
-  return(0);
+	fprintf(stderr, "file name [%d]: %s\n", arg, *fn);
+	return(0);
 }
 
 /*
@@ -72,41 +73,75 @@ int buildFilename(int argc, char **argv, int arg, char **fn) {
 */
 int openFile(char *fn, FILE **fp) {
 
-  // open files
-  *fp = fopen(fn, "r");
-  if(!*fp) {
-    fprintf(stderr, "unable to open file [%s]\n", fn);
-    return(1);
-  }
+	// open files
+	*fp = fopen(fn, "r");
+	if(!*fp) {
+		fprintf(stderr, "unable to open file [%s]\n", fn);
+		return(1);
+	}
 
-  return(0);
+	return(0);
 }
 
 
 /*
 **
 */
-int getNextLine(FILE *fp, char **line, size_t *len) {
+int getNextLine(FILE *fp, char **result, int *ln) {
 
-  ssize_t read;
-  char *newline;
+	ssize_t read;
+	char *ptr0, *ptr1, *ptr2;
+	int valid = 0;
+	size_t inputLen = 160;
 
-  if((read = getline(line, len, fp)) == -1) 
-    return(GET_NXLN);
+	ptr0 = (char *) malloc(inputLen);
 
-  // trim line
-  newline = trim(*line);
+	while(valid == 0) {		
+		
+		(*ln)++;
+		//fprintf(stderr, "((%d))\n", *ln);
+		if((read = getline(&ptr0, &inputLen, fp)) == -1) {
+			free(ptr0);
+			return(read);
+		}
 
-  // remove comments
+		if(!strlen(ptr0))
+			continue;
 
-  // remove spaces
+		//fprintf(stderr, "read{%lu}{%s}\n", strlen(ptr0), ptr0);
 
+		// inside trim
+		ptr1 = itrim(ptr0);
+		//fprintf(stderr, "trim[%d][%d][%lu][%s]\n", valid, *ln, strlen(ptr1), ptr1);
+		if(strlen(ptr1) == 0) {
+			//fprintf(stderr, "T-THERE\n");
+			free(ptr1);
+			continue;
+		}
 
-  // is the line empty
-  if(!strlen(newline)) return(getNextLine(fp, line, len));
-  //else fprintf(stderr, "<%s>\n", newline);
+		//fprintf(stderr, "--HERE\n");
 
-  return(read);
+		// remove cpp comments
+		ptr2 = removeCPPComments(ptr1);
+		//fprintf(stderr, "cmts[%d][%d][%lu][%s]\n", valid, *ln, strlen(ptr2), ptr2);
+		if(strlen(ptr2) == 0) {
+			//fprintf(stderr, "C-THERE\n");
+			free(ptr1);
+			free(ptr2);
+			continue;
+		}
+
+		valid = 1;
+	}
+
+	*result = (char *) malloc(strlen(ptr2) + 1);
+	strcpy(*result, ptr2);
+
+	free(ptr2);
+	free(ptr1);
+	free(ptr0);
+
+	return(read);
 }
 
 /*
@@ -114,57 +149,61 @@ int getNextLine(FILE *fp, char **line, size_t *len) {
 */
 int main(int argc, char **argv) {
 
-  FILE *fp1, *fp2;  // file pointer
-  char *fn1, *fn2;  // file name
-  int length1, length2;
+	FILE *fp1, *fp2;  // file pointer
+	char *fn1, *fn2;  // file name
+	int length1, length2;
 
-  if(validateArguments(argc, argv)) return(VAL_ARGS);
+	if(validateArguments(argc, argv)) return(VAL_ARGS);
 
-  if(buildFilename(argc, argv, 1, &fn1)) return(BUILD_FN);
-  if(buildFilename(argc, argv, 2, &fn2)) return(BUILD_FN);
+	if(buildFilename(argc, argv, 1, &fn1)) return(BUILD_FN);
+	if(buildFilename(argc, argv, 2, &fn2)) return(BUILD_FN);
 
-  if(openFile(fn1, &fp1)) return(OPEN__FN);
-  if(openFile(fn2, &fp2)) return(OPEN__FN);
+	if(openFile(fn1, &fp1)) return(OPEN__FN);
+	if(openFile(fn2, &fp2)) return(OPEN__FN);
 
 
-  char *line1 = NULL, *line2 = NULL;
-  size_t len1 = 0, len2 = 0;
-  int ln1 = 0, ln2 = 0; // line number
-  int loop1 = 1, loop2= 1;
+	char *line1 = NULL, *line2 = NULL;
+	//size_t len1 = 0, len2 = 0;
+	int ln1 = 0, ln2 = 0; // line number
+	int loop1 = 1, loop2= 1;
 
-  while(loop1 && loop2) {    
-    
-    ln1++; ln2++;
-    if(getNextLine(fp1, &line1, &len1) == -1) loop1=0;
-    if(getNextLine(fp2, &line2, &len2) == -1) loop2=0;
-    
-    if(strlen(line1)) fprintf(stderr, "[%d][%s]\t", ln1, line1);
-    if(strlen(line2)) fprintf(stderr, "[%d][%s]", ln2, line2);
-    fprintf(stderr, "\n");
-  }
+	fprintf(stderr, "\n--STARTING--\n");
 
-  fprintf(stderr, "[%d|%d]\n", loop1, loop2);
+	while(loop1 && loop2) {    
+		
+		if(getNextLine(fp1, &line1, &ln1) == -1) loop1=0;
+		if(getNextLine(fp2, &line2, &ln2) == -1) loop2=0;
+		
+		//if(strlen(line1)) fprintf(stderr, "[%d][%s]\n", ln1, line1);
+		//if(strlen(line2)) fprintf(stderr, "[%d][%s]\n", ln2, line2);
+		//fprintf(stderr, "\n");
 
-  /*
-  getNextLine(fp1, &line1, &len1);
+		if(strcmp(line1, line2)) {
+			break;
+		}
 
-  if(getNextLine(fp1, &line1, &len1)) 
-    fprintf(stdout, "\n[%02zu] %s", len1, line1);
-  else {
-    fprintf(stdout, "\nE:[%02zu] %s", len1, line1);
-  }
-  */
+		free(line1);
+		free(line2);
+	}
 
-  fprintf(stderr, "------------------\n");
+	if(loop1 && loop2) {
+		fprintf(stderr, "\n--FOUND DIFFERENCE--\n");
+		fprintf(stdout, "[%d][%s]\n", ln1, line1);
+		fprintf(stdout, "[%d][%s]\n", ln2, line2);
+	}
+	else
+		fprintf(stderr, "[[%d|%d]]\n\n", loop1, loop2);
 
-  fclose(fp1);
-  fclose(fp2);
+	fprintf(stderr, "\n");
 
-  if(line1) free(line1);
-  if(line2) free(line2);
+	fclose(fp1);
+	fclose(fp2);
 
-  free(fn1);
-  free(fn2);
+	//if(line1) free(line1);
+	//if(line2) free(line2);
 
-  return(0);
+	free(fn1);
+	free(fn2);
+
+	return(0);
 }
